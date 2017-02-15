3e16850a865c23980b489b80fd8ef3115ff51e59
ksindorf000 <ksindorf000@gmail.com>
2017-02-15 11:42:14 -0500

    Added line

--- MergeConflictFun/Program.cs
+++ MergeConflictFun/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +19,7 @@ namespace MergeConflictFun
             Console.WriteLine("Suh dude");
             Console.WriteLine("Mashed Potatoes");
             Console.WriteLine("Nick has weird socks.");
+            Console.WriteLine("Sorry for the disruption!");
         }
     }
 }
